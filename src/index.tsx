3de7181ecc2a366455f0d9e4263cf1e25497c8a2
Roger <rogerdr.rdr@gmail.com>
2022-09-23 08:26:14 -0300

    incluindo configuracoes pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -14,6 +16,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
