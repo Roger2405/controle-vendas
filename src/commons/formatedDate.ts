ea9dcf582cc34d75361437146f0f9c4b8cce7e6c
Roger <rogerdr.rdr@gmail.com>
2022-11-02 19:20:50 -0300

    alteracoes na exibicao da data

--- src/commons/formatedDate.ts
+++ src/commons/formatedDate.ts
@@ -6,11 +6,10 @@ export default function getFormatedDate(date?: string) {
     date ?
         starttime = new Date(date)
         :
-        starttime = new Date();
+        starttime = new Date().toISOString();
 
-    var isotime = new Date((new Date(starttime)).toISOString());
-    var fixedtime = new Date(isotime).toLocaleTimeString();//new Date(isotime.getTime() - (starttime.getTimezoneOffset() * 60000));
-    var fixeddate = new Date(isotime).toLocaleDateString().split('/').reverse().join('-');
+    var fixedtime = new Date(starttime).toLocaleTimeString();//new Date(starttime.getTime() - (starttime.getTimezoneOffset() * 60000));
+    var fixeddate = new Date(starttime).toLocaleDateString().split('/').reverse().join('-');
     //var formatedMysqlString = fixedtime.toISOString().slice(0, 19).replace('T', ' ');
     return `${fixeddate} ${fixedtime}`;
 }
--- src/pages/SaleDetails.tsx
+++ src/pages/SaleDetails.tsx
@@ -29,7 +29,7 @@ export default function SaleDetails() {
                         salesGroupedByDate.map(sales => {
                             return (
                                 <div>
-                                    <h2>{getFormatedDate(sales[0].data_venda).split(' ')[1]}</h2>
+                                    <h2>{getFormatedDate(sales[0].data_venda)}</h2>
                                     <OrderProducts hiddenOverflow={false} orderProducts={sales} />
                                 </div>
                             )
